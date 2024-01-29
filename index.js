58a220b6451223421024639d96b2c83431b2eb8f
abray <armando.ameri@studio.unibo.it>
2024-01-29 17:01:46 +0100

    firebase implementation

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, update, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import {appSettings ,DATABASE_NAME} from './firebaseConfig.js'
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const feedsInDB = ref(database, DATABASE_NAME)
+
 localStorage. clear();
 
 const feedDB = [
@@ -23,10 +31,12 @@ const feedDB = [
 
 const feeds = []
 
+// call only first time to push items in firebase
 function getFromDB() {
     return new Promise((resolve, reject) => {
         feedDB.forEach((feed) => {
             feeds.push(feed);
+            push(feedsInDB, feed)
             // console.log('feed pushed');
         });
         resolve(true);
@@ -38,16 +48,22 @@ const inputFromEl = document.getElementById("input-from")
 const inputToEl = document.getElementById("input-to")
 const addButtonEl = document.getElementById("publish-button")
 
-addButtonEl.addEventListener("click", function() {
+addButtonEl.addEventListener("click", async function() {
     let textAreaValue = textAreadEl.value
     let inputFromValue = inputFromEl.value
     let inputToValue = inputToEl.value
     
-    // console.log(textAreaValue)
-    // console.log(inputFromValue)
-    // console.log(inputToValue)
+    createCardDB(inputToValue,textAreaValue,inputFromValue)
+    clearInputFieldEl()
+})
 
+function clearInputFieldEl() {
+    textAreadEl.value = ''
+    inputFromEl.value = ''
+    inputToEl.value = ''
+}
 
+function createCardVisualization(inputToValue,textAreaValue,inputFromValue) {
     const container = document.getElementById("feeds")
 
     container.innerHTML += `<div class="card" id='card-${feeds.length}'>
@@ -61,7 +77,9 @@ addButtonEl.addEventListener("click", function() {
                         </div>
                     </div>
                 </div>`
+}
 
+function createCardDB(inputToValue,textAreaValue,inputFromValue) {
     const feed = {
         to: inputToValue,
         feed: textAreaValue,
@@ -71,92 +89,109 @@ addButtonEl.addEventListener("click", function() {
 
     feeds.push(feed);
     console.log(feeds)
-
-    const cardsEL = document.querySelectorAll('.card')
-    cardsEL.forEach((card,index) => {
-        const likeEl = document.getElementById(`card-like-${index}`)
-        const likeImgEl = document.getElementById(`like-${index}`)
-        const likeValueEl = document.getElementById(`like-value-${index}`)
-        likeEl.addEventListener("click", function() {
-            console.log(`pressed like of card ${index}`)
-            if (localStorage.getItem(`likeHasBeenClicked[${index}]`))
-                alert('You"ve already put like on this feed!')
-            else {
-                localStorage.setItem(`likeHasBeenClicked[${index}]`, true)
-                likeImgEl.setAttribute('src', './assets/heart-like-full.svg')
-                likeValueEl.textContent = Number(likeValueEl.textContent) + 1
-                feeds[index].likes += 1
-                console.log(feeds)
-            }
-        })
-    });
-})
-
-async function renderFeed() {
-    const container = document.getElementById("feeds")
-    let feedDOM = ""
-    for (let i = 0; i < feeds.length; i++) {
-
-        if (localStorage.getItem(`likeHasBeenClicked[${i}]`))
-            feedDOM += `<div class="card" id='card-${i}'>
-                            <h3 id='card-to-${i}'>To ${feeds[i].to}</h3>
-                            <p id='card-endorsement-${i}'>${feeds[i].feed}</p>
-                            <div class='card-footer'>
-                                <h3 id='card-from-${i}'>From ${feeds[i].from}</h3>
-                                <div class='card-like' id='card-like-${i}'>
-                                    <img alt='like' class='like' id='like-${i}' src='./assets/heart-like-full.svg' height="15px"/>
-                                    <span id='like-value-${i}'>${feeds[i].likes}</span>
-                                </div>
-                            </div>
-                        </div>`
-        else
-            feedDOM += `<div class="card" id='card-${i}'>
-                            <h3 id='card-to-${i}'>To ${feeds[i].to}</h3>
-                            <p id='card-endorsement-${i}'>${feeds[i].feed}</p>
-                            <div class='card-footer'>
-                                <h3 id='card-from-${i}'>From ${feeds[i].from}</h3>
-                                <div class='card-like' id='card-like-${i}'>
-                                    <img alt='like' class='like' id='like-${i}' src='./assets/heart-like-empty.svg' height="15px"/>
-                                    <span id='like-value-${i}'>${feeds[i].likes}</span>
-                                </div>
-                            </div>
-                        </div>`
-
-    }
-    return new Promise((resolve, reject) => {
-        container.innerHTML = feedDOM
-        resolve(true);
-    });
+    push(feedsInDB, feed)
 }
 
 
-async function main() {
-    await getFromDB()
-    await renderFeed()
 
-    if(localStorage.getItem("likeHasBeenClicked[0]"))
-        console.log(localStorage.getItem("likeHasBeenClicked[0]"))
-    else
-        console.log('Nothing in localStorage')
+/*
+feedDOM += `<div class="card" id='card-${i}'>
+                <h3 id='card-to-${i}'>To ${feeds[i].to}</h3>
+                <p id='card-endorsement-${i}'>${feeds[i].feed}</p>
+                <div class='card-footer'>
+                    <h3 id='card-from-${i}'>From ${feeds[i].from}</h3>
+                    <div class='card-like' id='card-like-${i}'>
+                        <img alt='like' class='like' id='like-${i}' src='./assets/heart-like-empty.svg' height="15px"/>
+                        <span id='like-value-${i}'>${feeds[i].likes}</span>
+                    </div>
+                </div>
+            </div>`
+*/
+
+
+const container = document.getElementById("feeds")
+
+function appendCardToFeedEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
     
-    const cardsEL = document.querySelectorAll('.card')
-    cardsEL.forEach((card,index) => {
-        const likeEl = document.getElementById(`card-like-${index}`)
-        const likeImgEl = document.getElementById(`like-${index}`)
-        const likeValueEl = document.getElementById(`like-value-${index}`)
-        likeEl.addEventListener("click", function() {
-            console.log(`pressed like of card ${index}`)
-            if (localStorage.getItem(`likeHasBeenClicked[${index}]`))
-                alert('You"ve already put like on this feed!')
-            else {
-                localStorage.setItem(`likeHasBeenClicked[${index}]`, true)
-                likeImgEl.setAttribute('src', './assets/heart-like-full.svg')
-                likeValueEl.textContent = Number(likeValueEl.textContent) + 1
-                feeds[index].likes += 1
-                console.log(feeds)
-            }
-        })
-    });
+    let cardDivEl = document.createElement("div")
+        cardDivEl.setAttribute('class', `card`)
+        cardDivEl.setAttribute('id', `card${itemID}`)
+
+    let cardToEl = document.createElement("h3")
+        cardToEl.setAttribute('id', `card-to${itemID}`)
+        cardToEl.textContent = 'To ' + itemValue.to
+        cardDivEl.append(cardToEl)
+
+    let cardEndorsementEl = document.createElement("p")
+        cardEndorsementEl.setAttribute('class', `card-endorsement`)
+        cardEndorsementEl.setAttribute('id', `card-endorsement${itemID}`)
+        cardEndorsementEl.textContent = itemValue.feed
+        cardDivEl.append(cardEndorsementEl)
+
+    let cardFootertEl = document.createElement("div")
+        cardFootertEl.setAttribute('class', `card-footer`)
+    let cardFromEl = document.createElement("h3")
+        cardFromEl.setAttribute('class', `card-from`)
+        cardFromEl.setAttribute('id', `card-from${itemID}`)
+        cardFromEl.textContent = 'From ' + itemValue.from
+        cardFootertEl.append(cardFromEl)
+    let cardLikeEl = document.createElement("div")
+        cardLikeEl.setAttribute('class', `card-like`)
+        cardLikeEl.setAttribute('id', `card-like${itemID}`)
+        let cardLikeImgEl = document.createElement("img")
+            cardLikeImgEl.setAttribute('alt', `like`)
+            cardLikeImgEl.setAttribute('class', `card-like-img`)
+            cardLikeImgEl.setAttribute('id', `card-like-img${itemID}`)
+            cardLikeImgEl.setAttribute('height', `15px`)
+            if (localStorage.getItem(`likeHasBeenClicked[${itemID}]`))
+                cardLikeImgEl.setAttribute('src', './assets/heart-like-full.svg')
+            else
+                cardLikeImgEl.setAttribute('src', './assets/heart-like-empty.svg')
+        cardLikeEl.append(cardLikeImgEl)
+        
+        let cardLikeValueEl = document.createElement("span")
+            cardLikeValueEl.setAttribute('id', `card-like-value${itemID}`)
+            cardLikeValueEl.textContent = itemValue.likes
+        cardLikeEl.append(cardLikeValueEl)
+
+    cardFootertEl.append(cardLikeEl)
+    cardDivEl.append(cardFootertEl)
+    container.append(cardDivEl)
+
+    cardLikeEl.addEventListener("click", function() {
+        console.log(`pressed like of card ${itemID}`)
+        if (localStorage.getItem(`likeHasBeenClicked[${itemID}]`)) {
+            alert(`You've already put like on this feed! I'm gonna remove it!`)
+            localStorage.removeItem(`likeHasBeenClicked[${itemID}]`)
+            cardLikeImgEl.setAttribute('src', './assets/heart-like-empty.svg')
+            cardLikeValueEl.textContent = Number(cardLikeValueEl.textContent) - 1
+            update(feedsInDB, {[itemID + '/likes'] : cardLikeValueEl.textContent})
+        } else {
+            localStorage.setItem(`likeHasBeenClicked[${itemID}]`, true)
+            cardLikeImgEl.setAttribute('src', './assets/heart-like-full.svg')
+            cardLikeValueEl.textContent = Number(cardLikeValueEl.textContent) + 1
+            update(feedsInDB, {[itemID + '/likes'] : cardLikeValueEl.textContent})
+        }
+    })
+}
+function clearFeedEl() {
+    container.innerHTML = ""
+}
+async function main() {
+    onValue(feedsInDB, function(snapshot) {
+        clearFeedEl()
+        let feedsArray = Object.entries(snapshot.val())
+        for (let i = 0; i < feedsArray.length; i++) {
+            let currentFeed = feedsArray[i]
+            let currentItemID = currentFeed[0]
+            let currentItemValue = currentFeed[1]
+            console.log(currentItemValue)
+            appendCardToFeedEl(currentFeed)
+        }
+    })
+
 
 }
 
